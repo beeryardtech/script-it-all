5a528774ffb81a3ec60e698933acc49175401dbc
Travis Goldie <tgoldie@gmail.com>
2015-10-02 16:42:48 -0400

    Updated bin ln paths

--- bin/basedepth.pl
+++ bin/basedepth.pl
@@ -1,7 +1,1 @@
-use strict;
-use warnings;
-
-use File::Spec;
-
-my @path = File::Spec->splitdir($ARGV[0]);
-print File::Spec->catdir(splice @path, -$ARGV[1]), "\n";
+/home/tgoldie/Dropbox/repos/beeryardtech/scripts/perl/basedepth.pl
